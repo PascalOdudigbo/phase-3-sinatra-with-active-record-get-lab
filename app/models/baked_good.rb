ba02987f66f83510a54776f37205a8a63392a357
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-08-31 20:10:35 +0300

    Done.

--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,3 +1,13 @@
 class BakedGood < ActiveRecord::Base
   # add association macro here
+  belongs_to :bakery 
+
+  def self.by_price
+    self.all.order(price: :desc)
+  end
+  
+  # def self.most_expensive
+  #   maximum_price = self.all.maximum(:price)
+  #   most_expensive_good = self.all.find_by(price: maximum_price)
+  # end
 end
