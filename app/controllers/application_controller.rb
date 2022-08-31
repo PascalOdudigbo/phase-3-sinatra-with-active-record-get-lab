5fa674a102dd9c68655722089add42bffd05ba1d
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-08-31 20:10:23 +0300

    commit tree 95e8bb7f7a7a9e38089c4672e5ed80f7a630e178

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,34 @@
 class ApplicationController < Sinatra::Base
-
+  set :default_content_type, 'application/json'
   # add routes
+  get '/' do
+    "Hello World"
+  end
+
+  get '/bakeries' do
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do 
+    bakeries = Bakery.all.find(params[:id])
+    bakeries.to_json(include: :baked_goods)
+
+  end
+
+  get '/baked_goods/by_price' do 
+    baked_goods = BakedGood.by_price
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do 
+    # most_expensive = BakedGood.most_expensive
+    # most_expensive.to_json
+
+    most_expensive = BakedGood.by_price.first
+    most_expensive.to_json
+  end
 
 end
